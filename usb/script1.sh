8916a324fb86587f276c2fcb1be157934c159164
Broskoll <broskoll.inc@gmail.com>
2016-10-12 11:32:08 +0200

    Update script1.sh

--- usb/script1.sh
+++ usb/script1.sh
@@ -51,87 +51,92 @@ genfstab -U -p /mnt >> /mnt/etc/fstab
 echo -e "Done \n"
 
 echo -e "Chroot \n"
-arch-chroot /mnt /bin/bash
-echo -e "Done /n"
 
-echo -e "Language \n"
-echo -e "Uncomment en_US.UTF-8"
-read -p "Press enter to continue"
-nano /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-echo -e "Done \n"
+echo `echo -e "Done /n"` >> archChroot.bash
 
-echo -e "Time \n"
-ln -s /usr/share/zoneinfo/Europe/Paris > /etc/localtime
-hwclock --systohc --utc
+echo `echo -e "Language \n"` >> archChroot.bash
+echo `echo -e "Uncomment en_US.UTF-8"` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `nano /etc/locale.gen` >> archChroot.bash
+echo `locale-gen` >> archChroot.bash
+echo `echo LANG=en_US.UTF-8 > /etc/locale.conf` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
 
-echo -e "Hostname \n"
-read -p "Enter a hostname : " hostnm
-echo $hostnm > /etc/hostname
-echo -e "Done \n"
+echo `echo -e "Time \n"` >> archChroot.bash
+echo `ln -s /usr/share/zoneinfo/Europe/Paris > /etc/localtime` >> archChroot.bash
+echo `hwclock --systohc --utc` >> archChroot.bash
 
-echo -e "Pacman & Yaourt \n"
-echo -e "Uncomment \"[multilib]\" and the next line"
-read -p "Press enter to continue"
-nano /etc/pacman.conf
-echo -e "[archlinuxfr]" >> /etc/pacman.conf
-echo -e "SigLevel = Never" >> /etc/pacman.conf
-echo -e "Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf
-echo -e "Update \"Pacman\" \n"
-pacman -Sy
-echo -e "Install \"Yaourt\" \n"
-pacman -S yaourt
-echo -e "Install \"Bash-completion\" \n"
-pacman -S bash-completion
-echo -e "Install \"Iw\", \"Wpa-supplicant\" and \"Dialog\" \n"
-pacman -S iw wpa-supplicant dialog
-echo -e "Done \n"
+echo `echo -e "Hostname \n"` >> archChroot.bash
+echo `read -p "Enter a hostname : " hostnm` >> archChroot.bash
+echo `echo $hostnm > /etc/hostname` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
 
-echo -e "Users \n"
-echo -e "Set root's password"
-read -p "Press enter to continue"
-passwd
-echo -e "Creation of the user"
-read -p "Enter a hostname : " usr
-useradd -m -g users -G wheel,storage,power -s /bin/bash $usr
-echo -e "Set user's password"
-read -p "Press enter to continue"
-passwd $usr
+echo `echo -e "Pacman & Yaourt \n"` >> archChroot.bash
+echo `echo -e "Uncomment \"[multilib]\" and the next line"` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `nano /etc/pacman.conf` >> archChroot.bash
+echo `echo -e "[archlinuxfr]" >> /etc/pacman.conf` >> archChroot.bash
+echo `echo -e "SigLevel = Never" >> /etc/pacman.conf` >> archChroot.bash
+echo `echo -e "Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf` >> archChroot.bash
+echo `echo -e "Update \"Pacman\" \n"` >> archChroot.bash
+echo `pacman -Sy` >> archChroot.bash
+echo `echo -e "Install \"Yaourt\" \n"` >> archChroot.bash
+echo `pacman -S yaourt` >> archChroot.bash
+echo `echo -e "Install \"Bash-completion\" \n"` >> archChroot.bash
+echo `pacman -S bash-completion` >> archChroot.bash
+echo `echo -e "Install \"Iw\", \"Wpa-supplicant\" and \"Dialog\" \n"` >> archChroot.bash
+echo `pacman -S iw wpa-supplicant dialog` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
 
-echo -e "Visudo \n"
-echo -e "Uncomment \"%wheel ALL=(ALL) ALL\" and add \"Defaults rootpw\" just after the uncommented line"
-read -p "Press enter to continue"
-EDITOR=nano visudo
-echo -e "Done \n"
+echo `echo -e "Users \n"` >> archChroot.bash
+echo `echo -e "Set root's password"` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `passwd` >> archChroot.bash
+echo `echo -e "Creation of the user"` >> archChroot.bash
+echo `read -p "Enter a hostname : " usr` >> archChroot.bash
+echo `useradd -m -g users -G wheel,storage,power -s /bin/bash $usr` >> archChroot.bash
+echo `echo -e "Set user's password"` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `passwd $usr` >> archChroot.bash
 
-echo -e "Mkinitcpio \n"
-echo -e "Write \"block\" before \"autodetect\" and remove the other \"block\""
-echo -e "It should end up like that : HOOKS=\"base udev block autodetect modconf filesystems keyboard fsck\""
-read -p "Press enter to continue"
-nano /etc/mkinitcpio.conf
-mkinitcpio -p linux
-echo -e "Done \n"
+echo `echo -e "Visudo \n"` >> archChroot.bash
+echo `echo -e "Uncomment \"%wheel ALL=(ALL) ALL\" and add \"Defaults rootpw\" just after the uncommented line"` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `EDITOR=nano visudo` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
 
-echo -e "Grub \n"
-pacman -S grub
-grub-install --boot-directory=/boot --recheck --debug --target=i386-pc /dev/$sd
-read -r -p "Did it fail ? [Y/n]" response
-response=${response,,} # tolower
-if [[ $response =~ ^(yes|y| ) ]]; then
-	grub-install --boot-directory=/boot --recheck --debug --force --target=i386-pc /dev/$sd
-fi
-grub-mkconfig -o /boot/grub/grub.cfg
-cd /boot/grub
-uuid=`blkid -o value -s UUID /dev/$sd2` 
-echo -e "LABEL Arch" >> menu.lst
-echo -e "    MENU LABEL Arch Linux" >> menu.lst
-echo -e "    LINUX ../vmlinuz-linux" >> menu.lst
-echo -e "    APPEND root=UUID=$uuid ro" >> menu.lst
-echo -e "    INITRD ../initramfs-linux.img" >> menu.lst
-echo -e "Done \n"
+echo `echo -e "Mkinitcpio \n"` >> archChroot.bash
+echo `echo -e "Write \"block\" before \"autodetect\" and remove the other \"block\""` >> archChroot.bash
+echo `echo -e "It should end up like that : HOOKS=\"base udev block autodetect modconf filesystems keyboard fsck\""` >> archChroot.bash
+echo `read -p "Press enter to continue"` >> archChroot.bash
+echo `nano /etc/mkinitcpio.conf` >> archChroot.bash
+echo `mkinitcpio -p linux` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
 
-exit
+echo `echo -e "Grub \n"` >> archChroot.bash
+echo `pacman -S grub` >> archChroot.bash
+echo `grub-install --boot-directory=/boot --recheck --debug --target=i386-pc /dev/$sd` >> archChroot.bash
+echo `read -r -p "Did it fail ? [Y/n]" response` >> archChroot.bash
+echo `response=${response,,}` >> archChroot.bash
+echo `if [[ $response =~ ^(yes|y| ) ]]; then` >> archChroot.bash
+echo `   grub-install --boot-directory=/boot --recheck --debug --force --target=i386-pc /dev/$sd` >> archChroot.bash
+echo `fi` >> archChroot.bash
+echo `grub-mkconfig -o /boot/grub/grub.cfg` >> archChroot.bash
+echo `cd /boot/grub` >> archChroot.bash
+echo `uuid=`blkid -o value -s UUID /dev/$sd2` ` >> archChroot.bash
+echo `echo -e "LABEL Arch" >> menu.lst` >> archChroot.bash
+echo `echo -e "    MENU LABEL Arch Linux" >> menu.lst` >> archChroot.bash
+echo `echo -e "    LINUX ../vmlinuz-linux" >> menu.lst` >> archChroot.bash
+echo `echo -e "    APPEND root=UUID=$uuid ro" >> menu.lst` >> archChroot.bash
+echo `echo -e "    INITRD ../initramfs-linux.img" >> menu.lst` >> archChroot.bash
+echo `echo -e "Done \n"` >> archChroot.bash
+
+echo `exit` >> archChroot.bash
+
+mv archChroot.bash /mnt
+chmod +x /mnt/archChroot.bash
+arch-chroot /mnt ./archChroot.bash
+
 umount -R /mnt
 echo -e "Reboot \n"
 reboot
